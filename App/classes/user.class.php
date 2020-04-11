74c140435ae0c55bb58b6afa2e03939eaebb1ad2
Victor <leofizzy3@gmail.com>
2020-04-12 00:00:20 +0100

    User Class: Adding User Method Working

--- App/classes/user.class.php
+++ App/classes/user.class.php
@@ -6,8 +6,17 @@ class User extends Dbh
     private $firstname, $lastname, $email, $password;
 
     // Class methods(Functions)
-    public function addUser()
+    public function addUser($_fname, $_lname, $_email, $_password)
     {
+        $this->firstname    = $this->cleanInput($_fname);
+        $this->lastname     = $this->cleanInput($_lname);
+        $this->email        = $this->cleanInput($_email);
+        $this->password     = $this->cleanInput($_password);
+
+        $sql = "INSERT INTO users(firstname, lastname, email, password) VALUES(?, ?, ?, ?)";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->bind_param("ssss", $this->firstname, $this->lastname, $this->email, $this->password);
+        $stmt->execute();
     }
 
     public function fetchUser($id)
@@ -24,5 +33,16 @@ class User extends Dbh
 
     public function editUser($id, $properties)
     {
+    }
+
+    // helper functions
+
+    private function cleanInput($input)
+    {
+        $input = trim($input);
+        $input = htmlspecialchars($input);
+        $input = stripslashes($input);
+        $input = $this->connect()->real_escape_string($input);
+        return $input;
     }
 }
