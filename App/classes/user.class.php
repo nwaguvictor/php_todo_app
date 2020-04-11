c49c8fcf60337eaf816e7b2fd36671d915a3e74c
Victor <leofizzy3@gmail.com>
2020-04-11 23:47:13 +0100

    Created the user class bootstrapping

---
+++ App/classes/user.class.php
@@ -0,0 +1,28 @@
+<?php
+require_once("db.class.php");
+class User extends Dbh
+{
+    // Class Properties(Variables)
+    private $firstname, $lastname, $email, $password;
+
+    // Class methods(Functions)
+    public function addUser()
+    {
+    }
+
+    public function fetchUser($id)
+    {
+    }
+
+    public function fetchAllUsers()
+    {
+    }
+
+    public function deleteUser($id)
+    {
+    }
+
+    public function editUser($id, $properties)
+    {
+    }
+}
