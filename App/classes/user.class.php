1f311d8fd1060a3a6ff654b0017b68b1cf5fa127
Victor <leofizzy3@gmail.com>
2020-04-12 01:56:50 +0100

    User Class: Completed, Ready to use

--- App/classes/user.class.php
+++ App/classes/user.class.php
@@ -3,7 +3,7 @@ require_once("db.class.php");
 class User extends Dbh
 {
     // Class Properties(Variables)
-    private $firstname, $lastname, $email, $password;
+    private $id, $firstname, $lastname, $email, $password;
 
     // Class methods(Functions)
     public function addUser($_fname, $_lname, $_email, $_password)
@@ -11,32 +11,78 @@ class User extends Dbh
         $this->firstname    = $this->cleanInput($_fname);
         $this->lastname     = $this->cleanInput($_lname);
         $this->email        = $this->cleanInput($_email);
-        $this->password     = $this->cleanInput($_password);
+        $this->password     = password_hash($this->cleanInput($_password), PASSWORD_DEFAULT);
 
         $sql = "INSERT INTO users(firstname, lastname, email, password) VALUES(?, ?, ?, ?)";
         $stmt = $this->connect()->prepare($sql);
         $stmt->bind_param("ssss", $this->firstname, $this->lastname, $this->email, $this->password);
-        $stmt->execute();
+        if ($stmt->execute()) {
+        } else {
+            // trigger_error("Query Error...", E_USER_ERROR);
+            echo "Error: Query Failed... Please Try Again";
+            return;
+        }
     }
 
-    public function fetchUser($id)
+    public function fetchUser($_id)
     {
+        $this->id = $this->cleanInput($_id);
+
+        $sql = "SELECT * FROM users WHERE id = '$this->id'";
+        if ($result = $this->connect()->query($sql)) {
+            return $result->fetch_object();
+        } else {
+            // trigger_error("Query Error...", E_USER_ERROR);
+            echo "Error: Query Failed... Please Try Again";
+            return;
+        }
     }
 
     public function fetchAllUsers()
     {
+        if ($result = $this->connect()->query("SELECT * FROM users")) {
+            while ($rows = $result->fetch_object()) {
+                $users[] = $rows;
+            }
+            return $users;
+        } else {
+            // trigger_error("Query Error...", E_USER_ERROR);
+            echo "Error: Query Failed... Please Try Again";
+            return;
+        }
     }
 
-    public function deleteUser($id)
+    public function deleteUser($_id)
     {
+        $this->id = $this->cleanInput($_id);
+        $sql = "DELETE FROM users WHERE id = '$this->id'";
+        if ($sql) {
+            $this->connect()->query($sql);
+        } else {
+            // trigger_error("Query Failed", E_USER_ERROR);
+            echo "Error: Query Failed... Please Try Again";
+            return;
+        }
     }
 
-    public function editUser($id, $properties)
+    public function updateUser($_id, $_fname, $_lname, $_email, $_password)
     {
+        $this->id           = $this->cleanInput($_id);
+        $this->firstname    = $this->cleanInput($_fname);
+        $this->lastname     = $this->cleanInput($_lname);
+        $this->email        = $this->cleanInput($_email);
+        $this->password     = password_hash($this->cleanInput($_password), PASSWORD_DEFAULT);
+
+        $sql = "UPDATE users SET firstname = ?, lastname = ?, email = ?, password = ? WHERE id = ?";
+        if ($sql) {
+            $stmt = $this->connect()->prepare($sql);
+            $stmt->bind_param("ssssi", $this->firstname, $this->lastname, $this->email, $this->password, $this->id);
+            $stmt->execute();
+        }
+        return;
     }
 
     // helper functions
-
     private function cleanInput($input)
     {
         $input = trim($input);
